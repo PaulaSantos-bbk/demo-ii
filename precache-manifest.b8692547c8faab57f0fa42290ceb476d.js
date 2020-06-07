e490711cf8da1fbc7ed21b1f4eedbe495662a0e7
unknown <pangeriz@outlook.com>
2020-06-07 13:07:24 +0100

    Updates

---
+++ precache-manifest.b8692547c8faab57f0fa42290ceb476d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4e7acdaa3500704c5b87e9deaa5ffc5",
+    "url": "/PaulaSantos-bbk/demo-ii/index.html"
+  },
+  {
+    "revision": "6f60d40782e3a7133975",
+    "url": "/PaulaSantos-bbk/demo-ii/static/css/main.816e69ba.chunk.css"
+  },
+  {
+    "revision": "dd99982ec587fb58d99f",
+    "url": "/PaulaSantos-bbk/demo-ii/static/js/2.8931250b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PaulaSantos-bbk/demo-ii/static/js/2.8931250b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f60d40782e3a7133975",
+    "url": "/PaulaSantos-bbk/demo-ii/static/js/main.85e67206.chunk.js"
+  },
+  {
+    "revision": "11ed542be45de22d0121",
+    "url": "/PaulaSantos-bbk/demo-ii/static/js/runtime-main.e0c99e57.js"
+  }
+]);
